6576417d7756ec10d64b32f3d8ecfadceb6c5bf1
bosshugoboss <alexhua0825@gmail.com>
2013-03-19 02:23:05 -0300

    Create ValidParenthese.java

---
+++ ValidParenthese.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isValid(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        Stack<Character> stack = new Stack<Character>();
+        for(int index = 0; index < s.length(); index ++){
+            if(isLeft(s.charAt(index)))
+               stack.push(s.charAt(index));
+            else{
+               if(stack.isEmpty()) return false;
+               char first = stack.pop();
+               if(!isPair(first,s.charAt(index)))
+               return false;
+            }
+        
+        }
+        if(stack.isEmpty()) return true;
+        return false;
+    }
+    
+    public boolean isLeft(char ch){
+        return ch == '{' || ch == '[' || ch == '(' ;
+    }
+    
+    public boolean isPair(char first, char second){
+        return (first == '(' && second == ')') || (first == '{' && second == '}') 
+               || (first == '[' && second == ']');
+    }
+}
