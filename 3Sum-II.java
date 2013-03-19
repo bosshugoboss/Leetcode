956644722a58cf4cd26a3d4616c672613d5a0787
bosshugoboss <alexhua0825@gmail.com>
2013-03-18 21:49:21 -0300

    Create 3Sum-II.java

---
+++ 3Sum-II.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int threeSumClosest(int[] num, int target) {
+           int result;
+           Arrays.sort(num);
+           result = num[0] + num[1] + num[2];
+           int nearest = Math.abs(result - target);
+           for(int index = 0; index < num.length; index ++){
+              int leftPointer = index + 1;
+              int rightPointer = num.length - 1;
+              int remainder = target - num[index];
+              while(rightPointer > leftPointer){
+                  if(remainder - num[leftPointer] == num[rightPointer]){
+                     return target;
+                  }
+                  nearest = Math.min(nearest,Math.abs(num[leftPointer] + num[rightPointer] - remainder));
+                  if(Math.abs(num[leftPointer] + num[rightPointer] - remainder) == nearest){
+                     result = num[index] + num[leftPointer] + num[rightPointer]; 
+                  }
+                  if(remainder - num[leftPointer] > num[rightPointer]){                
+                     leftPointer ++;
+                  }
+                  else{
+                     rightPointer --;
+                  }
+              }
+           }
+         return result;
+    }
