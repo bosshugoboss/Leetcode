31b6aaca01f406b567ed7d2d0323311cb2fb1f75
bosshugoboss <alexhua0825@gmail.com>
2013-03-19 02:24:17 -0300

    Update ValidParenthese.java

--- ValidParenthese.java
+++ ValidParenthese.java
@@ -1,3 +1,10 @@
+/**
+ * Given a string containing just the characters '(', ')', 
+ * '{', '}', '[' and ']', determine if the input string 
+ * is valid.The brackets must close in the correct order, "()" and 
+ * "()[]{}" are all valid but "(]" and "([)]" are not.
+ */ 
+ 
 public class Solution {
     public boolean isValid(String s) {
         // Start typing your Java solution below
