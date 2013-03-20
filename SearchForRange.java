9c8f5ce54627e0b9aab42d2369e8a7c4d8446fb0
bosshugoboss <alexhua0825@gmail.com>
2013-03-20 02:42:46 -0300

    Update SearchForRange.java

--- SearchForRange.java
+++ SearchForRange.java
@@ -1,2 +1,24 @@
 public class Solution {
     public int[] searchRange(int[] A, int target) {
+           int[] result = {-1, -1};
+           if(search(A, target) != -1 && A[search(A, target)] == target){
+               result[0] = search(A, target - 1) + 1;
+               result[1] = search(A, target);
+           }
+           return result;
+    } 
+    public int search(int[] A, int target){
+        int left = 0, right = A.length - 1;
+        int largestIndex = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(A[mid] > target)
+               right = mid - 1;
+            else if(A[mid] <= target){ 
+               left = mid + 1;
+               largestIndex = mid;
+            }   
+        }
+       return largestIndex;
+    }    
+}
