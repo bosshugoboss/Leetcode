64cd2855f17a16caf7c25c8db7de521afb2a1b60
bosshugoboss <alexhua0825@gmail.com>
2013-03-19 23:37:16 -0300

    Update KpairsLinkedList.java

--- KpairsLinkedList.java
+++ KpairsLinkedList.java
@@ -1,1 +1,26 @@
- 
+public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode firstPointer = head;
+        ListNode lastPointer = head;
+        ListNode newHead = head;
+        while(firstPointer != null){
+           for(int index = 1; index < k; index ++){
+              if(lastPointer == null) return newHead;
+              lastPointer = lastPointer.next;
+           }
+           if(firstPointer == head) newHead = lastPointer;
+           ListNode current = firstPointer;
+           ListNode prev = lastPointer.next;
+           while(current != lastPointer.next){
+              ListNode next = current.next;
+              current.next = prev;
+              prev = current;
+              current = next;              
+           }
+           firstPointer = lastPointer.next;
+        }
+        return newHead;
+    }
+}
