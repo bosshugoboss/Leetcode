35306a2321fca2c74b834e5844286aa8b24a4257
bosshugoboss <alexhua0825@gmail.com>
2013-03-20 02:57:15 -0300

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,1 +1,18 @@
- 
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+           int left = 0, right = A.length - 1;
+           int largestIndex = -1;
+           while(left <= right){
+             int mid = left + (right - left)/2;
+             if(A[mid] > target){
+                 right = mid - 1;
+             }
+             else{
+                 left = mid + 1;
+                 largestIndex = mid;
+             }
+         }
+         if(A[largestIndex] == target) return largestIndex;
+         else return largestIndex + 1;
+    }
+} 
