11867fe4a62dc6cf2b78e28ed66a0aec69b372f1
bosshugoboss <alexhua0825@gmail.com>
2013-03-21 19:18:38 -0300

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -1,7 +1,9 @@
+/**
+ * Given a number represented as an array of 
+ * digits, plus one to the number.
+ */ 
 public class Solution {
      public int[] plusOne(int[] digits) {
-         // Start typing your Java solution below
-         // DO NOT write main() function
          int length = digits.length;
          int index = 0;
          if(digits[length - 1] != 9){
