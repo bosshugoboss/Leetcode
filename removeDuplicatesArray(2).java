b92549d33dffff700a66998397de4a5a20b655d2
rhua <alexhua0825@gmail.com>
2013-03-23 01:03:06 -0300

    Update removeDuplicatesArray(2).java

--- removeDuplicatesArray(2).java
+++ removeDuplicatesArray(2).java
@@ -1,3 +1,10 @@
+/**
+ *  Follow up for "Remove Duplicates": 
+ * What if duplicates are allowed at most twice?
+ * For example, Given sorted array A = [1,1,1,2,2,3],
+ * Your function should return length = 5, and A is now [1,1,2,2,3]
+ * 
+ */ 
 public class Solution {
     public int removeDuplicates(int[] A) {
 
