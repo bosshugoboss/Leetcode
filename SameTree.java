8af2e8178c326236d545e1ff65386b7b34bcc250
rhua <alexhua0825@gmail.com>
2013-06-12 02:57:48 -0600

    Create SameTree.java

---
+++ SameTree.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public boolean isSameTree(TreeNode root1, TreeNode root2) {
+      if(root1 == null && root2 == null) return true;
+        if(root1 == null) return false;
+        if(root2 == null) return false;
+        if(root1.val != root2.val) return false;
+        return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+    }
+}
