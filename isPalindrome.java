2b263c5d813df4b5aeeb3bb4ff0edd447b037a3e
rhua <alexhua0825@gmail.com>
2013-08-31 12:10:04 -0400

    Update isPalindrome.java

--- isPalindrome.java
+++ isPalindrome.java
@@ -1,10 +1,16 @@
-public boolean isPalindrome(String s){
-		s= s.toLowerCase();
+public class Solution {
+    public boolean isPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+        s= s.toLowerCase();
         s= s.replaceAll("[^a-z0-9]","");
+        if(s.length() == 0) return true;
         int halfLength = (s.length() - 1)/2;
         for(int i = 0; i <= halfLength; i++){
         	if(s.charAt(i) != s.charAt(s.length() - 1 - i))
         		return false;
         }
         return true;
-	}
+    }
+}
