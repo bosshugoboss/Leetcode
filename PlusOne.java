04a5c633c932084c8dee7d7adda4eac310e931f6
rhua <alexhua0825@gmail.com>
2013-09-03 01:16:45 -0400

    Update PlusOne.java

--- PlusOne.java
+++ PlusOne.java
@@ -26,3 +26,29 @@ public class Solution {
          }
      }
  }
+ 
+ /* new */
+ public class Solution {
+     public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for(int i = digits.length - 1; i >= 0 ; i--){
+            int value = digits[i] + carry;
+        	if(value == 10){
+        		carry = 1;
+        		digits[i] = 0;
+        	}
+        	else{
+        		carry = 0;
+        		digits[i] = value;
+        		break;
+        	}
+        }
+        if(carry == 1){
+        	int[] newArray = new int[digits.length + 1];
+        	System.arraycopy(digits, 0, newArray, 1, digits.length);
+        	newArray[0] = 1;
+        	return newArray;
+        }
+        else return digits;
+    }
+}
