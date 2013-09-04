d4165779c10c2838e4a49e8f4c5e561a61c82db2
rhua <alexhua0825@gmail.com>
2013-09-03 22:22:32 -0400

    Update LargestRectangle.java

--- LargestRectangle.java
+++ LargestRectangle.java
@@ -24,3 +24,28 @@ public class Solution {
          return Math.max(Math.max(currentSum, leftSum), Math.max(currentSum,rightSum));
     }
 }
+
+
+/* new */
+public class Solution {
+    public int largestRectangleArea(int[] height) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        Stack<Integer> stack = new Stack<Integer>();
+        int maxArea = 0;
+        int i = 0;
+        int[] h = new int[height.length + 1];
+        h = Arrays.copyOf(height, height.length + 1);
+        height = h;
+        while(i < height.length){
+            if(stack.isEmpty() || height[stack.peek()] <= height[i]){
+                stack.push(i++);
+            }
+            else{
+                int temp = stack.pop();
+                maxArea = Math.max(maxArea, height[temp] * (stack.isEmpty()? i : i - stack.peek()) - 1);
+            }
+        }
+        return maxArea;
+    }
+}
