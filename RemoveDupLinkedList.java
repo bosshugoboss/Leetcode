8c0483e6353af255f32070b889aa168ef1460458
rhua <alexhua0825@gmail.com>
2013-09-03 22:43:10 -0400

    Update RemoveDupLinkedList.java

--- RemoveDupLinkedList.java
+++ RemoveDupLinkedList.java
@@ -29,3 +29,35 @@ public class Solution {
         return newHead;
     }
 }
+
+
+/* new */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+    
+        if(head == null) return head;
+        ListNode ahead = head;
+        ListNode current = head.next;
+        while(current != null){
+            if(ahead.val == current.val){
+                ahead.next = current.next;
+            }
+            else{
+                ahead = ahead.next;
+            }
+            current = current.next;      
+        }
+        return head;
+    }
+}
