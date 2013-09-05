c61a7fe055f21361f4cba3250efbf1027ec87164
rhua <alexhua0825@gmail.com>
2013-09-04 20:48:23 -0400

    Update RotateList.java

--- RotateList.java
+++ RotateList.java
@@ -1,9 +1,4 @@
-/**
- * Given a list, rotate the list to the right by k places, where k is non-negative.
- * For example.
- * Given 1->2->3->4->5->NULL and k = 2,return 4->5->1->2->3->NULL.
- */ 
-public class Solution {
+ public class Solution {
     public ListNode rotateRight(ListNode head, int n) {
            if(n == 0 || head == null) return head;
            ListNode bottom = head;
@@ -29,5 +24,32 @@ public class Solution {
            prev.next = null;
            bottom.next = head;
            return newHead;
+    }
+}
+
+/*new */
+public class Solution {
+    public ListNode rotateRight(ListNode head, int n) {
+        if(head == null || n == 0) return head;
+        int length = 0;
+        ListNode current = head;
+        ListNode tail = head;
+        while(current != null){
+            length ++;
+            if(current.next == null) tail = current;
+            current = current.next;
+        }
+        int rotate = n % length;
+        if(rotate == 0) return head;
+        current = head;
+        int rest = length - rotate;
+        while(rest > 1){
+            current = current.next;
+            rest --;
+        }
+        ListNode newHead = current.next;
+        current.next = null;
+        tail.next = head;
+        return newHead;
     }
 }
