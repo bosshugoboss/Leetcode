005eab15d37011a5f56fa83b5327c1fa492782d6
rhua <alexhua0825@gmail.com>
2013-09-05 00:37:03 -0400

    Update PermutationForArray.java

--- PermutationForArray.java
+++ PermutationForArray.java
@@ -34,3 +34,46 @@ public class Solution {
         return result;
     }
 }
+
+/*new */
+public class Solution {
+        public ArrayList<ArrayList<Integer>> permute(int[] num) {
+                // Start typing your Java solution below
+                // DO NOT write main() function
+	            ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	            ArrayList<ArrayList<Integer>> current = new ArrayList<ArrayList<Integer>>();
+	            permuteRec(num, 0, num.length, current, result);
+	            return result;
+	    }
+	        
+	    private void permuteRec(int[] num, int start, int end, ArrayList<ArrayList<Integer>> current, ArrayList<ArrayList<Integer>> result){
+	            if(start == end){
+	               for(ArrayList<Integer> array : current){
+	                   result.add(array);
+	               }
+	            }
+	            else{
+	                for(int i = start; i < end; i ++){
+	                    swap(num, start, i);
+	                    ArrayList<ArrayList<Integer>> newCurrent = new ArrayList<ArrayList<Integer>>();
+	                    if(current.size() == 0){
+	                    	ArrayList<Integer> p = new ArrayList<Integer>();
+	                    	p.add(num[start]);
+	                    	newCurrent.add(p);
+	                    }
+	                    for(ArrayList<Integer> array : current){
+	                        ArrayList<Integer> newA = new ArrayList<Integer>(array);
+	                        newA.add(num[start]);
+	                        newCurrent.add(newA);
+	                    }
+	                    permuteRec(num, start + 1, end, newCurrent, result);
+	                    swap(num, i, start);
+	                }
+	            }
+	        }
+	        private void swap(int[] num, int a, int b){
+	            int temp = num[a];
+	            num[a] = num[b];
+	            num[b] = temp;
+	        }
+}
