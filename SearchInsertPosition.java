e9906474d1b7d00a8eec3fbfa99f0032ae2bd573
rhua <alexhua0825@gmail.com>
2013-09-06 11:00:12 -0400

    Update SearchInsertPosition.java

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -17,3 +17,18 @@ public class Solution {
          else return largestIndex + 1;
     }
 } 
+/*new */
+public class Solution {
+    public int searchInsert(int[] A, int target) {
+        int start = 0;
+        int end = A.length - 1;
+        int mid = 0;
+        while(start <= end){
+            mid = (start + end)/2;
+            if(A[mid] == target) return mid;
+            if(A[mid] < target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return A[mid] < target ? mid + 1: mid ;
+    }
+}
