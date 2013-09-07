d01a1376f44f7fbad2c29904e59f2ed865bc249b
rhua <alexhua0825@gmail.com>
2013-09-07 17:44:05 -0400

    Update MissingPositive.java

--- MissingPositive.java
+++ MissingPositive.java
@@ -38,3 +38,47 @@ public class Solution {
         A[b] = temp;
     }
 }
+
+
+/* new */
+
+public class Solution {
+    public int firstMissingPositive(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int positiveLength = getAllPositive(A);
+        
+        for(int i = 0; i < positiveLength; i ++){
+            if(Math.abs(A[i]) - 1 < A.length)
+            A[Math.abs(A[i]) - 1] = - Math.abs(A[Math.abs(A[i]) - 1]);
+        }
+        
+        for(int i = 0; i < positiveLength; i ++){
+            if(A[i] > 0){
+              return i + 1;
+            }
+        } 
+        return positiveLength + 1;
+    }
+    
+    private int getAllPositive(int[] A){
+        int start = 0;
+        int end = A.length - 1;
+        while(start <= end){
+            if(A[start] <= 0){
+                swap(A, start, end);
+                end --;
+            }
+            else{
+                start ++;
+            }
+        }
+        return start;
+    }
+    
+    private void swap(int[] A, int i , int j){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
