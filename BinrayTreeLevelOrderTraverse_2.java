087821aa0403498438de3b32c5c591b8b223913d
rhua <alexhua0825@gmail.com>
2013-09-06 20:33:49 -0400

    Update BinrayTreeLevelOrderTraverse_2.java

--- BinrayTreeLevelOrderTraverse_2.java
+++ BinrayTreeLevelOrderTraverse_2.java
@@ -55,3 +55,34 @@ public class Solution {
         return new ArrayList<ArrayList<Integer>>(result); 
     }
 }
+
+/* new */
+public class Solution {
+    public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        Map<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
+        if(root == null) return result;
+        queue.offer(root);
+        map.put(root, 0);
+        while(!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            int level = map.get(temp);
+            if(level >= result.size()){
+                ArrayList<Integer> newA = new ArrayList<Integer>();
+                result.add(newA);
+            }
+               result.get(level).add(temp.val);
+            if(temp.left != null){
+                map.put(temp.left, level + 1);
+                queue.offer(temp.left);
+            }
+            if(temp.right != null){
+                map.put(temp.right, level + 1);
+                queue.offer(temp.right);
+            }
+        }
+        
+        return result;
+    }
+}
