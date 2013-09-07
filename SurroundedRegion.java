3923bb631aac6cbb7ac50d74a7039828264356f1
rhua <alexhua0825@gmail.com>
2013-09-07 17:21:03 -0400

    Update SurroundedRegion.java

--- SurroundedRegion.java
+++ SurroundedRegion.java
@@ -39,3 +39,54 @@ public class Solution {
             }
         }
     }
+    
+/* new */
+public class Solution {
+    public void solve(char[][] board) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(board == null || board.length == 0) return;
+        int width = board[0].length;
+        int height = board.length;
+        Queue<Integer> Xqueue = new LinkedList<Integer>();
+        Queue<Integer> Yqueue = new LinkedList<Integer>();
+        for(int i = 0; i < height; i ++){
+            for(int j = 0; j < width; j ++){
+                if(board[i][j] == 'O' && (i == 0 || i == height - 1 || j == 0 || j == width - 1)){
+                    Xqueue.offer(i); Yqueue.offer(j);
+                    board[i][j] = 'B';
+                    while(!Xqueue.isEmpty()){
+                        int x = Xqueue.poll();
+                        int y = Yqueue.poll();
+                        if(x - 1 >= 0 && board[x-1][y] == 'O'){
+                           Xqueue.offer(x-1);
+                           Yqueue.offer(y);
+                           board[x-1][y] = 'B';
+                        }
+                        if(x + 1 < height && board[x+1][y] == 'O'){
+                           Xqueue.offer(x+1);
+                           Yqueue.offer(y);
+                           board[x+1][y] = 'B';
+                        }
+                        if(y - 1 >= 0 && board[x][y-1] == 'O'){
+                           Xqueue.offer(x);
+                           Yqueue.offer(y-1);
+                           board[x][y-1] = 'B';
+                        }
+                        if(y + 1 < width && board[x][y+1] == 'O'){
+                           Xqueue.offer(x);
+                           Yqueue.offer(y+1);
+                           board[x][y+1] = 'B';
+                        }
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < height; i ++){
+            for(int j = 0; j < width; j ++){
+                if(board[i][j] == 'B') board[i][j] = 'O';
+                else board[i][j] = 'X';
+            }
+        }
+    }   
+}
